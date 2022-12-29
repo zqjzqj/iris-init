a05d7ad1425b620ce5763e3fec4a35a58a915185
zqijun <540173107@qq.com>
2022-12-29 10:02:35 +0800

    -

--- appWeb/routes/admin.go
+++ appWeb/routes/admin.go
@@ -16,7 +16,6 @@ func RegisterRoutes(app *iris.Application) {
 		tmpl = tmpl.Reload(true)
 	}
 	app.RegisterView(tmpl)
-	app.HandleDir("/static", "./static")
 	//重要！！！ admin所有需要设置权限的实际路由需要到控制器里去实际注册 并SetName 不然无法获取并生成到路由的权限--会被默认为不需要权限
 	//SetName格式 SetName("目录@菜单:按钮") 这里暂时只支持二级菜单
 	//b.Handle(http.MethodGet, "list", "GetList").SetName("用户组@用户列表")
@@ -24,7 +23,8 @@ func RegisterRoutes(app *iris.Application) {
 
 	//子按钮 子页面按钮点击跳转新的页面
 	//b.Handle(http.MethodPost, "delete", "PostDelete").SetName("用户组@用户列表:子页面:子页面")
-	party := app.Party("/") //可以定义子域名 .Subdomain("admin")
+	party := app.Party("/") //可定义子域名 .Subdomain("admin")
+	party.HandleDir("/static", "./static")
 	party.OnErrorCode(iris.StatusNotFound, func(ctx iris.Context) {
 		if !ctx.IsAjax() /*&& !strings.HasPrefix(ctx.Request().URL.Path, "/api/")*/ {
 			ctx.Redirect("/err?Msg=页面未找到", http.StatusFound)
