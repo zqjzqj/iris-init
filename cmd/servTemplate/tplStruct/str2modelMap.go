30efc77d037421f8400f004181c5cc3224e7a613
zqijun <540173107@qq.com>
2023-08-18 16:51:11 +0800

    -

--- cmd/servTemplate/tplStruct/str2modelMap.go
+++ cmd/servTemplate/tplStruct/str2modelMap.go
@@ -1,14 +1,3 @@
 package tplStruct
 
-import "iris-init/model"
-
-var Str2ModelMap = map[string]any{
-	"Course":             model.Course{},
-	"CourseSection":      model.CourseSection{},
-	"Institution":        model.Institution{},
-	"InstitutionAccount": model.InstitutionAccount{},
-	"Question":           model.Question{},
-	"QuestionAnswers":    model.QuestionAnswers{},
-	"Students":           model.Students{},
-	"Teacher":            model.Teacher{},
-}
+var Str2ModelMap = map[string]any{}
